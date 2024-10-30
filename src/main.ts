d2bb660cccaf57c157de8a5cc56e4d5829cf639e
Sootax <3jeunami82lekw@gmail.com>
2024-10-30 10:20:58 +0100

    Add support for writing and reading settings

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import { BrowserWindow, app } from 'electron';
 import path from 'path';
+import fs from 'fs';
 
 declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string;
 declare const MAIN_WINDOW_VITE_NAME: string;
@@ -13,10 +14,17 @@ if (require('electron-squirrel-startup')) {
 dotenv.config();
 
 const createWindow = () => {
+    const settings = readSettings();
+
+    const defaultBounds = { width: 800, height: 600, x: 0, y: 0 };
+    const bounds = settings?.bounds || defaultBounds;
+
     // Create the browser window.
     const mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: bounds.width,
+        height: bounds.height,
+        x: bounds.x,
+        y: bounds.y,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
         },
@@ -41,6 +49,12 @@ const createWindow = () => {
 
     // Remove the default menu bar
     mainWindow.setMenu(null);
+
+    mainWindow.on('close', () => {
+        const windowBounds = mainWindow.getBounds();
+        const newSettings = { ...settings, bounds: windowBounds};
+        writeSettings(newSettings);
+    })
 };
 
 // This method will be called when Electron has finished
@@ -67,3 +81,35 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and import them here.
+
+interface Settings {
+    firstLoad: boolean;
+    bounds: {
+        x: number;
+        y: number;
+        width: number;
+        height: number;
+    }
+}
+
+// Write the settings file
+function writeSettings(settings: Settings) {
+    try {
+        const data = JSON.stringify(settings, null, 2);
+        fs.writeFileSync(app.getPath('userData') + '/settings.json', data, 'utf8');
+        console.log('Settings saved successfully.');
+    } catch (err) {
+        console.error('Failed to save settings:', err);
+    }
+}
+
+// Read the settings file
+function readSettings() {
+    try {
+        const data = fs.readFileSync(app.getPath('userData') + '/settings.json', 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error('Failed to read settings:', err);
+        return null;
+    }
+}
